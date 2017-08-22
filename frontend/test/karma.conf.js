08a4a6d5cff27f903f5a03ea7c4e622e4b27745d
Vinicius Sanches <vinicius.sjc@gmail.com>
2017-08-22 06:48:22 -0300

    install kamra sinon and promise

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -2,10 +2,10 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     port: 9876,
-    frameworks: ['wiredep', 'jasmine'],
+    frameworks: ['wiredep', 'jasmine', 'sinon', 'promise'],
     colors: true,
     logLevel: config.LOG_INFO,
-    files: ['../public/app.js',            '../public/routes.js',            '../public/components/environment.js',            '../public/app/secret-friend/controller.js',            '../public/app/secret-friend/factory.js',            '../public/app/home/controller.js',            '../public/components/modal/controller.js',            '../public/components/navbar/directive.js',            '../public/app/secret-friend/create/modal-controller.js',            '../public/app/secret-friend/create/service.js',            '../public/app/secret-friend/friend/factory.js',            '../public/app/secret-friend/friend/service.js',            './**/*spec.js'],
+    files: ['../public/app.js',            '../public/routes.js',            '../public/components/environment.js',            '../public/app/home/controller.js',            '../public/app/secret-friend/controller.js',            '../public/app/secret-friend/factory.js',            '../public/components/modal/controller.js',            '../public/components/navbar/directive.js',            '../public/app/secret-friend/create/service.js',            '../public/app/secret-friend/friend/factory.js',            '../public/app/secret-friend/friend/service.js',            './**/*spec.js'],
     preprocessors: {
       '../public/!(bower_components|assets)/**/!(*.mock|*.spec).js': 'coverage'
     },
@@ -23,6 +23,8 @@ module.exports = function (config) {
       'karma-jasmine',
       'karma-phantomjs-launcher',
       'karma-coverage',
+      'karma-sinon',
+      'karma-promise',
       'karma-spec-reporter'
     ],
     coverageReporter: {
